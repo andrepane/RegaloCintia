dedcd330243d654e72008354b86170d29e1115ea
andrepane <156331723+andrepane@users.noreply.github.com>
2025-05-23 12:58:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ grad.addColorStop(1, "#777777");
 
   function checkScratchProgress() {
     drawCount++;
-    const limiteRascado = 400;
+    const limiteRascado = 450;
     if (drawCount >= limiteRascado) {
       canvas.classList.add("fade-out");
       canvas.style.pointerEvents = "none";
